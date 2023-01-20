6515d91d78131c2e37c98b4443d60772f454fa0d
livramateus <livra.matheus@gmail.com>
2023-01-19 22:10:19 -0300

    feat: improve download feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
+import fileDownload from "js-file-download";
 import "./App.css";
+import Logo from "./assets/logo.png";
 
 const requestOptions = {
   method: 'GET',
@@ -16,17 +18,21 @@ function App() {
   const [textInput, setTextInput] = useState('');
   const [id, setId] = useState('');
   const [response, setResponse] = useState(null);
+  const [disabled, setDisabled] = useState(false);
   
   useEffect(() => {
     if (id) {
       const fetchData = () => {
         requestOptions.params = { id };
           let interval = setInterval(async function() {
+            setDisabled(true);
             const res = await axios.request(requestOptions);
             
             if (res.status === 200 && res.data.status === "ok") {
               setResponse(res.data);
               clearInterval(interval);
+              setDisabled(false);
+              fileDownload(res.data.link, `${res.data.title}.mp3`);
             }
           }, 1000);
       }
@@ -43,23 +49,33 @@ function App() {
 
   return (
     <div id="app">
-      <h1>YouTube to MP3 Downloader</h1>
+      <img src={Logo} />
+      <h1>YT2MP3</h1>
 
-      <input
-        type="text"
-        value={textInput}
-        placeholder="YouTube link here..."
-        onChange={(e) => {
-          setTextInput(e.target.value);
+      <div>
+        <input
+          type="text"
+          value={textInput}
+          placeholder="YouTube link here..."
+          onChange={(e) => {
+            setTextInput(e.target.value);
+          }}
+        />
+
+        <span>It might take a moment to convert your video :)</span>  
+      </div>
+      
+      <button
+        onClick={() => {
+          const text = textInput.split("=")[1];
+          if (text) {
+            setId(text);
+          }
         }}
-      />
-
-      <button onClick={() => {
-        const text = textInput.split("=")[1];
-        if (text) {
-          setId(text);
-        }
-      }}>Download</button>
+        disabled={disabled}
+      >
+          Download
+        </button>
     </div>
   )
 }
