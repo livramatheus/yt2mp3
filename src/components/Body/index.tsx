c5f212ea6e81e3b55d5501e69316dd2899855690
livramateus <livra.matheus@gmail.com>
2023-01-31 20:17:53 -0300

    style: fix indent

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -7,19 +7,19 @@ function Body(props: TextInput) {
     const { textInput, setTextInput } = props;
  
     return (
-        <div id="body">
-            <input
-            type="text"
-            value={textInput}
-            placeholder="YouTube link here..."
-            onChange={(e) => {
-              setTextInput(e.target.value);
-            }}
-            />
+      <div id="body">
+        <input
+          type="text"
+          value={textInput}
+          placeholder="YouTube link here..."
+          onChange={(e) => {
+            setTextInput(e.target.value);
+          }}
+        />
 
-            <span>It might take a moment to convert your video :)</span>  
-      </div>
-    )
+        <span>It might take a moment to convert your video :)</span>  
+    </div>
+  )
 }
 
 export default Body;
